252b54b69c7150ff0b640826d547b9de97361da7
conradSadler <conradsadler2@gmail.com>
2025-03-02 11:13:51 -0700

    V.1 complete and useable

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-theme: jekyll-theme-minimal
-title: Octocat's homepage
-description: Bookmark this to keep an eye on my project updates!
+This app. is intended to let users explore the wonders that compound intrest can do to capital with time.
--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-const express = require('express');
-const app = express();
-const handlebars = require('express-handlebars');
-const path = require('path');
-const bodyParser = require('body-parser');
-const session = require('express-session');
 
-const hbs = handlebars.create({extname: 'hbs', layoutsDir: __dirname + '../frontend/public/layout', partialsDir: __dirname + '../frontend/public/partial',});
-app.engine('hbs',hbs.engine);
-app.set('view engine','hbs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(bodyParser.json());
-// set Session
-app.use(
-  session({
-    saveUninitialized: true,
-    resave: true,
-  })
-);
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-const user = {
-    finalAmount: undefined,
-};
-/*
-Might NEED:
+let itemToBeFormated = document.getElementById("principle");  //This finds the number that the user enterded as initial investment
+/**
+ * This event listener listens for when a key is released when the user is typing in initial investment 
+ * text box. This input is parsed as an integer in base 10 useing a REGEX \D to get only digits. Then this integer is formated with toLocaleString(), which 
+ * adds commas at significant places like 1,000.
+ */ 
+itemToBeFormated.addEventListener('keyup', function(evt){
+    var parsedPrinciple = parseInt(this.value.replace(/\D/g,''),10);
+    parsedPrinciple.toLocaleString();
+}, false);
 
-app.get('/login', (req, res) => {
-  res.render('pages/login');
-});
-
-*/
-
-app.post('/calculate'), (req, res) => {
+/**
+ * This function generates the values that will be displayed in the chart beased off of the compound intrest equation which is compounded once per year
+ * @param {*} value the equation
+ * @param {*} xStart the starting year
+ * @param {*} xEnd the ending year
+ * @param {*} step the interval of years. default 1
+ * @param {*} xValues the empty array which will store the years
+ * @param {*} yValues the empty array which will store the appreciated investment
+ */
+const generateData = (value,xStart,xEnd,step = 1,xValues,yValues) =>
+{
+    for(let x = xStart; x <= xEnd; x += step)
+    {
+        yValues.push(Number((eval(value)).toFixed(2)));  //calculating appreciated investment with equation, rounded to two decimal points
+        xValues.push(x);
+    }
+}
+/**
+ * This function parses the user input from the page and then generates a chart(chart.js) using the user provided information.
+ */
+const calculate = () => {
     try
     {
-        const initialAmount = req.body.initialAmount;
-        const theAPY = req.body.apy;
-        const years = req.body.time;
+        const initialAmount = document.getElementById("principle").value;
+        const theAPY = document.getElementById("apy").value;
+        const years = document.getElementById("time").value;
 
         let parsedInitialAmount = "";
         let parsedInitialAPY = "";
@@ -47,14 +43,15 @@ app.post('/calculate'), (req, res) => {
 
         let inDecimal = false;
 
-        for(let i = 0; i < initialAmount.lenth; i++)
+        for(let i = 0; i < initialAmount.length; i++)
         {
             if(initialAmount.charAt(i) == '.' || (initialAmount.charAt(i) >= '0' && initialAmount.charAt(i) <= '9'))
             {
                 parsedInitialAmount+=initialAmount.charAt(i);
             }
         }
-        for(let i = 0; i < theAPY.lenth; i++)
+
+        for(let i = 0; i < theAPY.length; i++)
         {
             if(i == 0 && theAPY.charAt(i) == '.')
             {
@@ -67,7 +64,7 @@ app.post('/calculate'), (req, res) => {
             }
         }
 
-        for(let i = 0; i < years.lenth; i++)
+        for(let i = 0; i < years.length; i++)
         {
             if(years.charAt(i) == '.' || (years.charAt(i) >= '0' && years.charAt(i) <= '9'))
             {
@@ -87,15 +84,52 @@ app.post('/calculate'), (req, res) => {
         {
             initialAPYFloat = parseFloat(parsedInitialAPY)/100;
         }
-        const finalAmountFloat = initialAmountFloat * Math.pow( (1+(initialAPYFloat/1)) , (1*yearsFloat) );
-        user.finalAmount = finalAmountFloat;
-        req.session.user = user;
-        req.session.save();
+        //This conditional below removes any previous chart that was created becasue of previous times the user clicked "calculate"
+        let chartOrig = document.getElementById("compoundIntrestChart");
+        if(chartOrig)
+        {
+            chartOrig.remove();
+        }
 
-        res.redirect('/result');
+        let chart = document.createElement("canvas");  //creating canvas element where the chart will reside
+        chart.id = "compoundIntrestChart";
+        chart.style = "max-width: 100%;height:auto;";
+        chart.classList = "rounded shadow";
+        document.getElementById("chartLoc").appendChild(chart);
+        let xValues = [];
+        let yValues = [];
+        generateData( (initialAmountFloat+" * "+(1+(initialAPYFloat/1))+"** x") , 0, yearsFloat, 1,xValues,yValues);
+
+        new Chart("compoundIntrestChart", {
+            type: "line",
+            data: {
+              labels: xValues,
+              datasets: [{
+                fill: false,
+                pointRadius: 3,
+                borderColor: "#007BFF",
+                data: yValues
+              }]
+            },    
+            options: {
+              legend: {display: false},
+              scales: {
+                yAxes: [{
+                    scaleLabel:{ display: true, labelString: "US Dollars ($)"}
+                }],
+                xAxes:[{
+                    scaleLabel:{ display: true, labelString: "Years"}
+                }]
+              },
+              title: {
+                display: true,
+                text: "Total Savings",
+              }
+            }
+          });
     }
     catch (err)
     {
-        res.status(400).json({error:err,});
+        console.log("Error: " + err)
     }
 }
